c9390724e4a4db4a2700602e2b221a0e05a1db85
ggoral <goral.gonzalo@gmail.com>
2014-08-25 09:42:17 -0300

    overriding register form

--- src/FBA/CoreBundle/Entity/User.php
+++ src/FBA/CoreBundle/Entity/User.php
@@ -5,6 +5,7 @@ namespace FBA\CoreBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity
@@ -21,9 +22,18 @@ class User extends BaseUser
 
     /**
      * @var string
-     *
      * @ORM\Column(name="name", type="string", length=255)
+     *
+     * @Assert\NotBlank(message="Please enter your name.", groups={"Registration", "Profile"})
+     * @Assert\Length(
+     *     min=3,
+     *     max="255",
+     *     minMessage="The name is too short.",
+     *     maxMessage="The name is too long.",
+     *     groups={"Registration", "Profile"}
+     * )
      */
+
     private $name;
 
     /**
@@ -55,6 +65,7 @@ class User extends BaseUser
      * @return integer
      */
     private $laboratorio;
+    
     public function setLaboratorio(\FBA\CoreBundle\Entity\Laboratorio $laboratorio)
     {
         $this->laboratorio = $laboratorio;
